bea5b65c606aaceef6b1895e58659a53380bb28b
Yong Jian Ming <jianming1993@gmail.com>
2024-05-30 22:16:17 +0800

    Formatting

--- sram.asm
+++ sram.asm
@@ -1,10 +1,6 @@
 SECTION "SaveVariables", SRAM[$a000]
 
-sUnknown_a000:: ; $a000
-    ds $1818
+sUnknown_a000:: ds $1818 ; $a000
 
-sChecksumLsb:: ; $b818
-    db
-
-sChecksumMsb:: ; $b819
-    db
+sChecksumLsb:: db ; $b818
+sChecksumMsb:: db ; $b819
